6e39ad5c52668848aff66b0479fa0f1cd5774d27
aiden2244 <aidenm2244@gmail.com>
2023-05-10 01:14:26 -0400

    I'm getting warm on opening the file...

--- libPseudoFS/pdos_open.c
+++ libPseudoFS/pdos_open.c
@@ -1,5 +1,84 @@
 #include "pdosfilesys.h"
 
 PDOS_FILE *pdos_open(const char* fname, const char* mode) {
-    return NULL;
+
+    // validate that the name is less than the max name
+    if (strlen(fname) > MAXNAME) {
+        printf("Error: File name is too long.\n");
+        return NULL;
+    }
+
+    // validate that the mode is either "r", "w", or "rw"
+    if (strcmp(mode, "r") != 0 && strcmp(mode, "w") != 0 && strcmp(mode, "rw") != 0) {
+        printf("Error: Invalid mode.\n");
+        return NULL;
+    }
+    
+    // create the file to be returned
+    PDOS_FILE *file = NULL;
+
+    // read the directory block into memory
+    int shm_fd = shm_open(disk_name, O_RDWR, S_IRUSR | S_IWUSR);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(1);
+    }
+
+    // we need to read the entire admin block into memory since mmap must be page aligned
+    DISK_BLOCK *admin_block = mmap(NULL, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (admin_block == MAP_FAILED) {
+        perror("mmap");
+        close(shm_fd);
+        exit(1);
+    }
+
+    // use pointer arithmetic to get to the fourth block (directory block)
+    DISK_BLOCK *dir_block = (DISK_BLOCK *) ((char *) admin_block + 3 * BLOCK_SIZE);
+
+    // check the directory entries of the directory block for the file
+    int i;
+    for (i = 0; i < MAX_NUM_DIRECTORIES_ENTRIES; i++) {
+        if (strcmp(dir_block->dir.dir_entry_list[i].name, fname) == 0) {
+            break;
+        }
+    }
+
+    // if i == MAX_NUM_DIRECTORIES_ENTRIES, then the file was not found
+    if (i == MAX_NUM_DIRECTORIES_ENTRIES) {
+        printf("File not found\n");
+
+        // get the next available entry
+        int next_entry = dir_block->dir.nextEntry;
+
+        // if the next entry is the max number of entries, then the directory is full
+        if (next_entry == MAX_NUM_DIRECTORIES_ENTRIES) {
+            printf("Directory is full\n");
+            return NULL;
+        }
+
+        
+
+        return NULL;
+
+
+    }
+    else {
+        printf("File found\n");
+
+    }
+
+    // close the shared memory
+    if (munmap(admin_block, BLOCK_SIZE) == -1) {
+        perror("munmap");
+        close(shm_fd);
+        exit(1);
+    }
+
+    if (close(shm_fd) == -1) {
+        perror("close");
+        exit(1);
+    }
+
+    return file;
+
 }
